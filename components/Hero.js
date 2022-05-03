b8f1c7787fa6f739c3d2762b1abe8c41f763b558
Willian Ferreira <willian@Willians-MacBook-Air.local>
2022-05-03 16:58:40 -0400

    hero

--- components/Hero.js
+++ components/Hero.js
@@ -1,19 +1,23 @@
+import { Anchor, findPosition } from "./CallToAction";
+import bgtop from "../public/imgs/bg_top.jpg";
+
 export default function Hero({ children }) {
   return (
     <>
-      <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+    <img src={bgtop.src} className="absolute -z-10 -mt-36" />
+      <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28 mb-36 lg:mb-56">
         <div className="sm:text-center lg:text-left">
-          <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            <span className="block xl:inline">Data to enrich your</span><br />
-            <span className="block text-indigo-600 xl:inline">online business</span>
+          <h1 className="text-xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-3xl lg:w-1/2">
+            <span className="block xl:inline">Tenha até 3x mais lucro em sua operação de dropshipping. Processando pedidos fora do aliexpress.</span><br />
           </h1>
-          <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+          <p className="mt-1 text-xl font-thin pb-5 text-gray-500 sm:mt-3 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-0 md:text-xl lg:mx-0">Economize no pagamento dos seus produtos e potencialize seu lucro com a ferramenta mais poderosa de processamento de pedidos do mercado.</p>
           <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-            <div className="rounded-md shadow">
-              <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"> Get started </a>
-            </div>
-            <div className="mt-3 sm:mt-0 sm:ml-3">
-              <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"> Live demo </a>
+            <div className="w-full lg:w-1/2">
+              <Anchor onClick={() => window.scrollTo({
+                top: findPosition(document.getElementById("ctaElement")),
+                left: 0,
+                behavior: 'smooth'
+              })} text={"Quero lucrar mais"} />
             </div>
           </div>
         </div>
